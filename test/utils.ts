80eb76167b79ae3cdcd81a76dc4cfaf58f16dec7
Ajitomi Daisuke <dajiaji@gmail.com>
2023-09-03 08:25:35 +0900

    Add test for kyber768.

---
+++ test/utils.ts
@@ -0,0 +1,29 @@
+export const isDeno = () => typeof Deno !== "undefined";
+
+export function testVectorPath(): string {
+  if (isDeno()) {
+    return "./test/vectors";
+  }
+  return "../../test/vectors";
+}
+
+export function hexToBytes(v: string): Uint8Array {
+  if (v.length === 0) {
+    return new Uint8Array([]);
+  }
+  const res = v.match(/[\da-f]{2}/gi);
+  if (res == null) {
+    throw new Error("Not hex string.");
+  }
+  return new Uint8Array(res.map(function (h) {
+    return parseInt(h, 16);
+  }));
+}
+
+export function bytesToHex(v: Uint8Array): string {
+  return [...v].map((x) => x.toString(16).padStart(2, "0")).join("");
+}
+
+export function hexToDec(hexString: string): number {
+  return parseInt(hexString, 16);
+}
