a4ee3b30f283d952b8ad401ac11a2f4d27a47737
Ajitomi Daisuke <dajiaji@gmail.com>
2023-09-07 01:08:07 +0900

    Refine KyberBase impl.

--- src/kyber512.ts
+++ src/kyber512.ts
@@ -3,9 +3,9 @@
  * which was deveploped under the MIT licence below:
  * https://github.com/antontutoveanu/crystals-kyber-javascript/blob/main/LICENSE
  */
-import { N, Q } from "./consts.ts";
+import { N } from "./consts.ts";
 import { KyberBase } from "./kyberBase.ts";
-import { byte, byteopsLoad24, int16, prf, uint16, uint32 } from "./utils.ts";
+import { byteopsLoad24, int16, prf } from "./utils.ts";
 
 export class Kyber512 extends KyberBase {
   protected _k = 2;
@@ -29,85 +29,14 @@ export class Kyber512 extends KyberBase {
   ): Array<Array<number>> {
     const r = new Array<Array<number>>(size);
     for (let i = 0; i < size; i++) {
-      r[i] = byteopsCbd1(prf(this._eta1 * N / 4, sigma, offset), this._eta1);
+      r[i] = byteopsCbd(prf(this._eta1 * N / 4, sigma, offset), this._eta1);
       offset++;
-    }
-    return r;
-  }
-
-  protected override _compressU(
-    r: Uint8Array,
-    u: Array<Array<number>>,
-  ): Uint8Array {
-    const t = new Array<number>(4);
-    for (let rr = 0, i = 0; i < this._k; i++) {
-      for (let j = 0; j < N / 4; j++) {
-        for (let k = 0; k < 4; k++) {
-          // parse {0,...,3328} to {0,...,1023}
-          t[k] = (((u[i][4 * j + k] << 10) + Q / 2) / Q) &
-            0b1111111111;
-        }
-        // converts 4 12-bit coefficients {0,...,3328} to 5 8-bit bytes {0,...,255}
-        // 48 bits down to 40 bits per block
-        r[rr++] = byte(t[0] >> 0);
-        r[rr++] = byte((t[0] >> 8) | (t[1] << 2));
-        r[rr++] = byte((t[1] >> 6) | (t[2] << 4));
-        r[rr++] = byte((t[2] >> 4) | (t[3] << 6));
-        r[rr++] = byte(t[3] >> 2);
-      }
-    }
-    return r;
-  }
-
-  protected override _compressV(r: Uint8Array, v: Array<number>): Uint8Array {
-    // const r = new Uint8Array(128);
-    const t = new Uint8Array(8);
-    for (let rr = 0, i = 0; i < N / 8; i++) {
-      for (let j = 0; j < 8; j++) {
-        t[j] = byte(((v[8 * i + j] << 4) + Q / 2) / Q) & 0b1111;
-      }
-      r[rr++] = t[0] | (t[1] << 4);
-      r[rr++] = t[2] | (t[3] << 4);
-      r[rr++] = t[4] | (t[5] << 4);
-      r[rr++] = t[6] | (t[7] << 4);
-    }
-    return r;
-  }
-
-  protected override _decompressU(a: Uint8Array): Array<Array<number>> {
-    const r = new Array<Array<number>>(this._k);
-    for (let i = 0; i < this._k; i++) {
-      r[i] = new Array<number>(384);
-    }
-    const t = new Array<number>(4);
-    for (let aa = 0, i = 0; i < this._k; i++) {
-      for (let j = 0; j < N / 4; j++) {
-        t[0] = (uint16(a[aa + 0]) >> 0) | (uint16(a[aa + 1]) << 8);
-        t[1] = (uint16(a[aa + 1]) >> 2) | (uint16(a[aa + 2]) << 6);
-        t[2] = (uint16(a[aa + 2]) >> 4) | (uint16(a[aa + 3]) << 4);
-        t[3] = (uint16(a[aa + 3]) >> 6) | (uint16(a[aa + 4]) << 2);
-        aa = aa + 5;
-        for (let k = 0; k < 4; k++) {
-          r[i][4 * j + k] = int16(
-            (((uint32(t[k] & 0x3FF)) * (uint32(Q))) + 512) >> 10,
-          );
-        }
-      }
-    }
-    return r;
-  }
-
-  protected override _decompressV(a: Uint8Array): Array<number> {
-    const r = new Array<number>(384);
-    for (let aa = 0, i = 0; i < N / 2; i++, aa++) {
-      r[2 * i + 0] = int16(((uint16(a[aa] & 15) * uint16(Q)) + 8) >> 4);
-      r[2 * i + 1] = int16(((uint16(a[aa] >> 4) * uint16(Q)) + 8) >> 4);
     }
     return r;
   }
 }
 
-function byteopsCbd1(buf: Uint8Array, eta: number): Array<number> {
+function byteopsCbd(buf: Uint8Array, eta: number): Array<number> {
   let t, d;
   let a, b;
   const r = new Array<number>(384).fill(0);
--- src/kyberBase.ts
+++ src/kyberBase.ts
@@ -22,6 +22,7 @@ import {
   loadCrypto,
   prf,
   uint16,
+  uint32,
 } from "./utils.ts";
 
 export class KyberBase {
@@ -335,29 +336,84 @@ export class KyberBase {
     return r;
   }
 
-  // compress1 lossily compresses and serializes a vector of polynomials.
-  protected _compressU(_r: Uint8Array, _u: Array<Array<number>>): Uint8Array {
-    throw new Error("Use Kyber512/768/1024.");
+  // compressU lossily compresses and serializes a vector of polynomials.
+  protected _compressU(
+    r: Uint8Array,
+    u: Array<Array<number>>,
+  ): Uint8Array {
+    const t = new Array<number>(4);
+    for (let rr = 0, i = 0; i < this._k; i++) {
+      for (let j = 0; j < N / 4; j++) {
+        for (let k = 0; k < 4; k++) {
+          // parse {0,...,3328} to {0,...,1023}
+          t[k] = (((u[i][4 * j + k] << 10) + Q / 2) / Q) &
+            0b1111111111;
+        }
+        // converts 4 12-bit coefficients {0,...,3328} to 5 8-bit bytes {0,...,255}
+        // 48 bits down to 40 bits per block
+        r[rr++] = byte(t[0] >> 0);
+        r[rr++] = byte((t[0] >> 8) | (t[1] << 2));
+        r[rr++] = byte((t[1] >> 6) | (t[2] << 4));
+        r[rr++] = byte((t[2] >> 4) | (t[3] << 6));
+        r[rr++] = byte(t[3] >> 2);
+      }
+    }
+    return r;
   }
 
-  // compress2 lossily compresses and subsequently serializes a polynomial.
-  protected _compressV(_r: Uint8Array, _v: Array<number>): Uint8Array {
-    throw new Error("Use Kyber512/768/1024.");
+  // compressV lossily compresses and subsequently serializes a polynomial.
+  protected _compressV(r: Uint8Array, v: Array<number>): Uint8Array {
+    // const r = new Uint8Array(128);
+    const t = new Uint8Array(8);
+    for (let rr = 0, i = 0; i < N / 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        t[j] = byte(((v[8 * i + j] << 4) + Q / 2) / Q) & 0b1111;
+      }
+      r[rr++] = t[0] | (t[1] << 4);
+      r[rr++] = t[2] | (t[3] << 4);
+      r[rr++] = t[4] | (t[5] << 4);
+      r[rr++] = t[6] | (t[7] << 4);
+    }
+    return r;
   }
 
-  // decompress1 de-serializes and decompresses a vector of polynomials and
+  // decompressU de-serializes and decompresses a vector of polynomials and
   // represents the approximate inverse of compress1. Since compression is lossy,
   // the results of decompression will may not match the original vector of polynomials.
-  protected _decompressU(_a: Uint8Array): Array<Array<number>> {
-    throw new Error("Use Kyber512/768/1024.");
+  protected _decompressU(a: Uint8Array): Array<Array<number>> {
+    const r = new Array<Array<number>>(this._k);
+    for (let i = 0; i < this._k; i++) {
+      r[i] = new Array<number>(384);
+    }
+    const t = new Array<number>(4);
+    for (let aa = 0, i = 0; i < this._k; i++) {
+      for (let j = 0; j < N / 4; j++) {
+        t[0] = (uint16(a[aa + 0]) >> 0) | (uint16(a[aa + 1]) << 8);
+        t[1] = (uint16(a[aa + 1]) >> 2) | (uint16(a[aa + 2]) << 6);
+        t[2] = (uint16(a[aa + 2]) >> 4) | (uint16(a[aa + 3]) << 4);
+        t[3] = (uint16(a[aa + 3]) >> 6) | (uint16(a[aa + 4]) << 2);
+        aa = aa + 5;
+        for (let k = 0; k < 4; k++) {
+          r[i][4 * j + k] = int16(
+            (((uint32(t[k] & 0x3FF)) * (uint32(Q))) + 512) >> 10,
+          );
+        }
+      }
+    }
+    return r;
   }
 
-  // decompress2 de-serializes and subsequently decompresses a polynomial,
+  // decompressV de-serializes and subsequently decompresses a polynomial,
   // representing the approximate inverse of compress2.
   // Note that compression is lossy, and thus decompression will not match the
   // original input.
-  protected _decompressV(_a: Uint8Array): Array<number> {
-    throw new Error("Use Kyber512/768/1024.");
+  protected _decompressV(a: Uint8Array): Array<number> {
+    const r = new Array<number>(384);
+    for (let aa = 0, i = 0; i < N / 2; i++, aa++) {
+      r[2 * i + 0] = int16(((uint16(a[aa] & 15) * uint16(Q)) + 8) >> 4);
+      r[2 * i + 1] = int16(((uint16(a[aa] >> 4) * uint16(Q)) + 8) >> 4);
+    }
+    return r;
   }
 }
 
