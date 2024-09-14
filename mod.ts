ff676e5561dee3496438497a49c23e92c3d27589
Ajitomi Daisuke <dajiaji@gmail.com>
2024-09-15 08:30:09 +0900

    Sync with the public archive version of @openpgpjs/crystals-kyber-js.

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-export { KyberError } from "./src/errors.ts";
-export { Kyber512 } from "./src/kyber512.ts";
-export { Kyber768 } from "./src/kyber768.ts";
-export { Kyber1024 } from "./src/kyber1024.ts";
+export { MlKemError } from "./src/errors.ts";
+export { MlKem512 } from "./src/kyber512.ts";
+export { MlKem768 } from "./src/kyber768.ts";
+export { MlKem1024 } from "./src/kyber1024.ts";
---
+++ src/deps.ts
@@ -0,0 +1,6 @@
+export {
+  sha3_256,
+  sha3_512,
+  shake128,
+  shake256,
+} from "npm:@noble/hashes@1.4.0/sha3";
