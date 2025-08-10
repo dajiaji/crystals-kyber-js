3a82b49a95e9e6531f8012705aee64693c194455
Ajitomi Daisuke <dajiaji@gmail.com>
2025-08-10 23:39:10 +0900

    Disclose sha3 functions as utilities.

--- mod.ts
+++ mod.ts
@@ -2,3 +2,5 @@ export { MlKemError } from "./src/errors.ts";
 export { MlKem512 } from "./src/mlKem512.ts";
 export { MlKem768 } from "./src/mlKem768.ts";
 export { MlKem1024 } from "./src/mlKem1024.ts";
+
+export { sha3_256, sha3_512, shake128, shake256 } from "./src/sha3/sha3.ts";
